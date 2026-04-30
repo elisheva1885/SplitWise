52e21d8ee5e48a3144a25b63a8b4fcb5e17599a1
elisheva1885 <elisheva1885@gmail.com>
2026-04-30 11:53:25 +0300

    fix the file names

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter} from "react-router-dom"
-import { HomePage } from "../pages/home-page"
-import { UserPage } from "../pages/user-page"
-import { GroupPage } from "../pages/group-page"
+import { HomePage } from "../pages/home.page"
+import { UserPage } from "../pages/user.page"
+import { GroupPage } from "../pages/group.page"
 import { Layout } from "../components/layout"
 
 const router = createBrowserRouter([
